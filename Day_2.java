d4321c805ad4b9ab3d1cbb9b1124279ec7fd1e38
varundeeep761786 <88708092+varundeeep761786@users.noreply.github.com>
2021-08-11 14:23:52 +0530

    Add files via upload

---
+++ Day_2.java
@@ -0,0 +1,13 @@
+package line1;
+public class Sumofnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = Integer.parseInt(args[0]);
+	        int b = Integer.parseInt(args[1]);
+	        int sum = a + b;
+	        System.out.println("Sum is " + sum);
+	    }
+	
+	}
+
